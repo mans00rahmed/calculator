8ae8db541fdaf6523616f3d9e101771333b37e71
Mansoor Ahmed <52007922+mansoorahmed-asterics@users.noreply.github.com>
2020-07-05 05:58:14 +0500

    first commit

--- app.js
+++ app.js
@@ -2,23 +2,35 @@ function getNumber(num){
     var result= document.getElementById("result")
     result.value += num
 }
-
 function clearResult(){
     var result= document.getElementById("result");
     result.value = ""
 }
-
 function Result(){
     var result= document.getElementById("result");
     result.value = eval(result.value)
 }
-
 function square(){
     var result= document.getElementById("result");
     result.value = result.value*result.value
 }
-
 function root(){
     var result= document.getElementById("result");
-    result.value = Math.sqrt(result.value)
+    result.value=Math.sqrt(result.value)
+}
+function sin(){
+    var result=document.getElementById("result")
+    result.value=Math.sin(result.value)
+}
+function sininv(){
+    var result=document.getElementById("result")
+    result.value=1/Math.sin(result.value)
+}
+function log(){
+    var result=document.getElementById("result")
+    result.value=Math.log(result.value)
+}
+function back(){
+    var result=document.getElementById("result")
+    result.value=result.value.slice(0,-1)
 }
